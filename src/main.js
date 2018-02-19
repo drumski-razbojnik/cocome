575c0e53d04233801013931f1b548fb61e21b9f7
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-02-19 22:29:33 +0100

    Added global components. To be improved later.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//Components
+import topMenu from './components/shared/top-menu'
+
+import transactions from './components/balance/transactions'
+import transactionRow from './components/balance/transaction-row'
+import transactionHeaderRow from './components/balance/transaction-header-row'
+
+Vue.component('top-menu', topMenu)
+
+Vue.component('transactions', transactions)
+Vue.component('transaction-row', transactionRow)
+Vue.component('transaction-header-row', transactionHeaderRow)
+
 new Vue({
   el: '#app',
   render: h => h(App),
