4d3d2e688aa57b9be7314b20ce45aec1a8a73aa9
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-02-19 22:26:37 +0100

    Added initial layout for the sidebars.

---
+++ src/components/balance/menu-friend.vue
@@ -0,0 +1,68 @@
+<template>
+  <aside class="balance__aside">
+    <div class="balance__aside__section balance__aside__section--user">
+      <!-- <div class="balance__aside__section--user__info">
+        <h2 class="balance__aside__section--user__info__title">Lana Franić</h2>
+      </div> -->
+      <div class="balance__aside__section--user__avatar">
+        <img class="balance__aside__section--user__avatar__photo" src="https://scontent-vie1-1.xx.fbcdn.net/v/t31.0-8/26850023_10156092070106880_3337621856397472840_o.jpg?oh=0cd729191f0a1341b3b595a6598560cc&oe=5B0A6CE9" />
+      </div>
+    </div>
+    <div class="balance__aside__section balance__aside__section--total">
+      <div class="balance__aside__section--total__type">
+        <p>Ja dugujem</p>
+        <p>Ona duguje</p>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.balance__aside {
+  &__section {
+    :not(:first-child) {
+      margin-top: 30px;
+    }
+
+    &--user {
+      padding-bottom: 25px;
+      border-bottom: 1px solid #fff;
+      
+      &__avatar {
+        overflow: hidden;
+
+        margin: 0 auto;
+        height: 140px;
+        width: 140px;
+
+        border: 2px solid #fff;
+        border-radius: 50%;
+
+        &__photo {
+          height: 100%;
+          width: 100%;
+
+          transform: scale(1.05);
+          transition: transform 0.25s ease-in-out;
+
+          &:hover {
+            transform: scale(1);
+          }
+        }
+      }
+
+      &__info {
+        &__title {
+          font-size: 1rem;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
+
---
+++ src/components/balance/menu-friends.vue
@@ -0,0 +1,96 @@
+<template>
+   <aside class="balance__aside">
+      <nav class="balance__aside__nav">
+         <h2 class="balance__aside__nav__title">Obitelj</h2>
+         <ul class="balance__aside__nav__menu">
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Robi</a>
+            </li>
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Tin</a>
+            </li>
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Mama</a>
+            </li>
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Tata</a>
+            </li>
+         </ul>
+      </nav>
+
+      <nav class="balance__aside__nav">
+         <h2 class="balance__aside__nav__title">Prijatelji</h2>
+         <ul class="balance__aside__nav__menu">
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Rick</a>
+            </li>
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Patrick</a>
+            </li>
+         </ul>
+      </nav>
+
+      <nav class="balance__aside__nav">
+         <h2 class="balance__aside__nav__title">Cigani</h2>
+         <ul class="balance__aside__nav__menu">
+            <li class="balance__aside__nav__menu__item">
+               <a class="balance__aside__nav__menu__item__link" href="#">Hamdija I.</a>
+            </li>
+         </ul>
+      </nav>
+   </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.balance__aside {
+  &__nav {
+    &:not(:first-child) {
+      margin-top: 30px;
+    }
+
+    &__title {
+      font-size: 1rem;
+      font-weight: 600;
+
+      margin: 0;
+      padding: 0 0 10px 0;
+    }
+
+    &__subtitle {
+      font-size: 0.9rem;
+      font-weight: 300;
+
+      margin: 0;
+      padding: 0 0 10px 0;
+    }
+
+    &__menu {
+      margin: 0;
+      padding: 0;
+
+      list-style-type: none;
+      &__item {
+        background-color: #fff;
+        border-radius: 2.5px;
+
+        margin: 5px 0;
+        padding: 0;
+
+        &__link {
+          display: block;
+          padding: 5px 10px;
+
+          color: #333;
+          font-size: 0.85rem;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
+</style>
+
