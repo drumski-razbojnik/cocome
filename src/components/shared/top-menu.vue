5cc304b91e3dddfcd48ae70e9ae55e8348fe5306
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-02-19 22:28:15 +0100

    Added initial dummy layout for the site header.

---
+++ src/components/shared/top-menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <div class="header__section header__section--brand">Cocome</div>
+    <div class="header__section header__section--middle"></div>
+    <div class="header__section header__section--right"></div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "static/variables";
+
+.header {
+  display:flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+
+  padding:10px 40px;
+
+  background-color: #96C0B7;
+
+
+  &__section {
+    &--brand {
+      color:#fff;
+      font-size:2rem;
+      font-family: $fontLogo;
+
+      flex-basis:20%;
+    }
+
+    &--middle {
+      flex-basis:60%;
+    }
+
+    &--right {
+      flex-basis:20%;
+    }
+  }
+}
+</style>
+
