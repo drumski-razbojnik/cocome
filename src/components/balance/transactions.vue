594eddaa4658a8ccc96f653694cce2ed9424cf40
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-04-10 21:01:22 +0200

    Outlined the layout of the transactions component

--- src/components/balance/transactions.vue
+++ src/components/balance/transactions.vue
@@ -1,11 +1,34 @@
 <template>
-   <section class="transactions">
-      <transaction-header-row></transaction-header-row>
-      <transaction-row v-for="i in 5" :key="i"></transaction-row>
-   </section>
+  <section class="transactions" :class="{ 'transactions--closed':!transactionGroup.expanded}">
+    <transaction-row v-for="t in transactionGroup.transactions" :key="t.id" :transaction="t"></transaction-row>
+  </section>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  props: {
+    transactionGroup: {
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {
+  }
+};
+</script>
 
+
+<style lang="scss">
+.transactions {
+  overflow: hidden;
+  transition: height ease-in-out 0.2s;
+  &--closed {
+    height: 0;
+  }
+}
 </style>
 
