a9beff61a1353d9d498705447f8998de6fd1c40a
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-04-10 20:54:50 +0200

    Added transaction group component

---
+++ src/components/balance/transaction-group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="transaction-group">
+    <transaction-header-row :transactionGroup="transactionGroup"></transaction-header-row>
+    <transactions :transactionGroup="transactionGroup"></transactions>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transactionGroup: {
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+.transaction-group {
+  border-bottom: 1px solid rgb(133, 133, 133);
+}
+</style>
+
