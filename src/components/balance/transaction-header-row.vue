bd3aadade3ab5fa44f9714e60567a9999200b587
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-04-10 20:57:25 +0200

    Outlined a layout for the transaction header row component

--- src/components/balance/transaction-header-row.vue
+++ src/components/balance/transaction-header-row.vue
@@ -1,15 +1,40 @@
 <template>
-   <header class="transaction-header">
-      <div class="transaction-header__column transaction-header__column--date">
-         <h2 class="transaction-header__column__value transaction-header__column--date__value">February 2018.</h2>
-      </div>
-      <!-- <div class="transaction-header__column transaction-header__column--actions">
-         <span class="transaction-header__column__icon transaction-header__column--actions__icon">
-            <i class="material-icons">keyboard_arrow_down</i>
-         </span>
-      </div> -->
-   </header>
+  <header class="transaction-header">
+    <div class="transaction-header__column transaction-header__column--date">
+      <h2 class="transaction-header__column__value transaction-header__column--date__value">
+        {{transactionGroup.name}}
+      </h2>
+    </div>
+    <div class="transaction-header__column transaction-header__column--actions">
+      <span class="transaction-header__column__icon transaction-header__column--actions__icon" @click="toggleGroupVisibility">
+        <i class="material-icons" v-if="transactionGroup.expanded">keyboard_arrow_down</i>
+        <i class="material-icons" v-else>keyboard_arrow_up</i>
+      </span>
+    </div>
+  </header>
 </template>
+
+<script>
+export default {
+  props: {
+    transactionGroup: {
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    toggleGroupVisibility() {
+      this.$store.dispatch(
+        "transactions/toggleTransactionGroup",
+        this.transactionGroup.id
+      );
+    }
+  }
+};
+</script>
 
 
 <style lang="scss">
