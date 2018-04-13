8a995261f4c410fd97beaa8ecf0069e0799ac78c
Hrvoje Fadiga <hrvoje.fadiga@gmail.com>
2018-04-13 16:57:29 +0200

    Rewired logic to work without the old transactions component

--- src/components/balance/transaction-group.vue
+++ src/components/balance/transaction-group.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="transaction-group">
     <transaction-header-row :transactionGroup="transactionGroup"></transaction-header-row>
-    <transactions :transactionGroup="transactionGroup"></transactions>
+    <div v-if="transactionGroup.expanded">
+      <transaction-row v-for="transaction in transactionGroup.transactions" :key="transaction.id" :transaction="transaction"></transaction-row>
+    </div>
+
   </div>
 </template>
 
